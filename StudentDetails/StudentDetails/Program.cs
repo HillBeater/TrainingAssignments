8b9d03f564cadbd3a6f740ddbdea3dd968beab1e
Anurag <anurag.ad32@gmail.com>
2022-10-12 22:48:15 +0530

    StudentDetails Assignment

---
+++ StudentDetails/StudentDetails/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_new
+{
+    internal class Program : Student
+    {
+        static void Main(string[] args)
+        {
+            Hashtable myhash = new Hashtable();
+            Student student = new Student();
+
+            while (true)
+            {
+                Console.WriteLine("Press 1 for insertion");
+                Console.WriteLine("Press 2 for deletion");
+                Console.WriteLine("Press 3 for searching");
+                Console.WriteLine("Press 4 for retrieving");
+                Console.WriteLine();
+                int choice;
+                Console.Write("Enter your Choice: ");
+                
+                choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Enter the Details of student");
+                            Console.WriteLine("Enter the student ID");
+                            student.Id = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter the student Name");
+                            student.Name = Convert.ToString(Console.ReadLine());
+                            Console.WriteLine("Enter the student Standard");
+                            student.Standard = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter the student RollNumber");
+                            student.RollNumber = Convert.ToInt32(Console.ReadLine());
+                            student.add(student.Id, student.Name, student.Standard, student.RollNumber);
+                            myhash.Add(student.RollNumber, student);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Enter the Roll Number of student whose record has to be deleted");
+                            var StdRollNumber = Convert.ToInt32(Console.ReadLine());
+                            myhash.Remove(StdRollNumber);
+                            Console.WriteLine("All the Details of Roll Number " + StdRollNumber + " has been deleted");
+                            Console.WriteLine();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Enter the Roll Number whose record has been search");
+                            var searchStd = Convert.ToInt32(Console.ReadLine());
+                            foreach (Student std in myhash.Values)
+                            {
+                                if (myhash.ContainsKey(searchStd))
+                                {
+                                    Console.WriteLine("Student Details Avilable");
+                                    Console.WriteLine();
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Student Details Unavilable");
+                                    Console.WriteLine();
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            foreach (Student std in myhash.Values)
+                            {
+                                Console.WriteLine("Student Details");
+                                Console.WriteLine("Student ID " + std.Id);
+                                Console.WriteLine("Student Name " + std.Name);
+                                Console.WriteLine("Student Standard " + std.Standard);
+                                Console.WriteLine("Student RollNumber " + std.RollNumber);
+                                Console.WriteLine();
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            return;
+                        }
+                }
+
+
+            }
+        }
+    }
+}
+    
+
---
+++ StudentDetails/StudentDetails/StudentClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_new
+{
+   internal class Student
+    {
+
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Standard { get; set; }
+        public int RollNumber { get; set; }
+       
+
+        public void add(int id, string name, int standard, int rollnumber)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Standard = standard;
+            this.RollNumber = rollnumber;
+
+            Console.WriteLine("Student Details");
+            Console.WriteLine("Student ID " + Id);
+            Console.WriteLine("Student Name " + Name);
+            Console.WriteLine("Student Standard " + Standard);
+            Console.WriteLine("Student RollNumber " + RollNumber);
+            Console.WriteLine();
+        }
+
+    }
+}
+
+
+
+    
+
+
+   
+
